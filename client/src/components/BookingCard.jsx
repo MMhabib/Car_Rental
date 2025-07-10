fcb8aa19c111c85b9ff27f399a95f482a6e54dcc
Habib <habibleo861@gmail.com>
2025-07-10 18:24:50 +0600

    Bookings

---
+++ client/src/components/BookingCard.jsx
@@ -0,0 +1,51 @@
+import { assets } from "../assets/assets";
+
+const BookingCard = ({ booking,index }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-bordercolor rounded-lg mt-5 first:mt-12">
+      {/* car image */}
+      <div className="md:col-span-1">
+        <div className="rounded-md overflow-hidden">
+          <img
+            src={booking.car.image}
+            alt=""
+            className="w-full h-auto aspect-video object-cover"
+          />
+        </div>
+        <p className="text-lg font-medium mt-2">
+          {booking.car.brand}
+          {booking.car.model}
+        </p>
+        <p className="text-gray-500">
+          {booking.car.year} . {booking.car.category} . {booking.car.location}
+        </p>
+      </div>
+      {/* booking info */}
+      <div className="md:col-span-2">
+        <div className="flex items-center gap-2 ">
+            <p className="px-3 py-1.5 bg-light rounded">Booking #{index+1}</p>
+            <p className={`px-3 py-1 text-xs rounded-full ${booking.status === 'confirmed' ? 'bg-green-400/15 text-green-600 ' : 'bg-red-400/15 text-red-600'}`}>{booking.status}</p>
+        </div>
+<div className="flex items-start gap-2 mt-3">
+    <img src={assets.calendar_icon_colored} alt="" className="w-4 h-4 mt-1"/>
+    <div>
+        <p className="text-gray-500">Rental Period</p>
+        <p> {booking.pickupDate.split('T')[0]} To {booking.returnDate.split('T')[0]}</p>
+    </div>
+</div>
+<div className="flex items-start gap-2 mt-3">
+    <img src={assets.location_icon_colored} alt="" className="w-4 h-4 mt-1"/>
+    <div>
+        <p className="text-gray-500">Pickup Location</p>
+        <p> {booking.car.location}</p>
+    </div>
+</div>
+<div>
+
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingCard;
--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import {  dummyMyBookingsData } from "../assets/assets";
+import Title from "../components/Title";
+import BookingCard from "../components/BookingCard";
 
 
 const MyBookings = () => {
+
+    const [bookings,setBookings] = useState([])
+
+    const fetchBookingData= async()=>{
+        setBookings(dummyMyBookingsData)
+    }
+
+    useEffect(()=>{
+        fetchBookingData()
+    },[])
+
     return (
-        <div>
-            
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
+            <Title title='My Bookings' subTitle={'View and manage your car bookings'}/>
+
+            <div >
+                {
+                    bookings.map((booking,index)=><BookingCard  key={booking._id} booking={booking} index={index}/>)
+                }
+            </div>
         </div>
     );
 };
